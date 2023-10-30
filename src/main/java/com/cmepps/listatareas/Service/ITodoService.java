afef4aab0cae978f2cb421bc696252cd5312c888
rocioph <rocioph70@gmail.com>
2023-10-30 22:06:18 +0100

    practica5 completada

---
+++ src/main/java/com/cmepps/listatareas/Service/ITodoService.java
@@ -0,0 +1,24 @@
+package com.cmepps.listatareas.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import com.cmepps.listatareas.model.Todo;
+
+
+
+public interface ITodoService {
+
+    List < Todo > getTodosByUser(String user);
+
+    Optional < Todo > getTodoById(long id);
+
+    void updateTodo(Todo todo);
+
+    void addTodo(String name, String desc, Date targetDate, boolean isDone);
+
+    void deleteTodo(long id);
+
+    void saveTodo(Todo todo);
+}
